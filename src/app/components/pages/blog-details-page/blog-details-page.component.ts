bd25f9f80e50e9e0137b8d07b21d1daf12f62f25
Wichan Thumthong <chankmit@gmail.com>
2023-08-24 13:04:14 +0700

    new build

--- src/app/components/pages/blog-details-page/blog-details-page.component.ts
+++ src/app/components/pages/blog-details-page/blog-details-page.component.ts
@@ -11,6 +11,7 @@ export class BlogDetailsPageComponent {
   newsid:any;
   newsname:any; 
   content:any;
+  newsdetail='';
   constructor(private activeRoute: ActivatedRoute, public ApiService:ApiService) {
     this.activeRoute.params.subscribe((routeParams:any) => { 
       this.newsid=routeParams.id; 
@@ -21,7 +22,14 @@ export class BlogDetailsPageComponent {
   
   callData(newsid:any){ 
     this.ApiService.getNewsdetail(newsid).subscribe((results:any)=>{ 
-      this.content=results.data  
+      this.content=results.data ;
+      this.newsdetail = this.content?.attributes.detail;
+      this.newsdetail = this.newsdetail.replace('"/uploads/', '"http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+      this.newsdetail = this.newsdetail.replace('"/uploads/', '"http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+      this.newsdetail = this.newsdetail.replace(', /uploads/', ', http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+      this.newsdetail = this.newsdetail.replace(', /uploads/', ', http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+      this.newsdetail = this.newsdetail.replace(', /uploads/', ', http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+      //console.log(this.newsdetail);
     })
   }
 }
--- src/app/components/pages/detail-page/detail-page.component.ts
+++ src/app/components/pages/detail-page/detail-page.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from 'src/app/api.service';
-
+import { ViewEncapsulation } from '@angular/core'
 @Component({
   selector: 'app-detail-page',
   templateUrl: './detail-page.component.html',
-  styleUrls: ['./detail-page.component.scss']
+  styleUrls: ['./detail-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DetailPageComponent implements OnInit {
   pagename: any;
@@ -24,10 +25,18 @@ export class DetailPageComponent implements OnInit {
 
   callData(pagename:any){
     this.ApiService.getpageDetail(pagename).subscribe((results:any)=>{ 
+      //console.log(results);
       if(results.data.length>0){
         this.pagedata=results.data[0] 
         this.pageshow=this.pagedata.attributes.show
-        this.pagedetail=this.pagedata.attributes.detail
+        this.pagedetail=this.pagedata.attributes.detail  
+        this.pagedetail = this.pagedetail.replace('"/uploads/', '"http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+        this.pagedetail = this.pagedetail.replace('"/uploads/', '"http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+        this.pagedetail = this.pagedetail.replace(', /uploads/', ', http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+        this.pagedetail = this.pagedetail.replace(', /uploads/', ', http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+        this.pagedetail = this.pagedetail.replace(', /uploads/', ', http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
+        //console.log(this.newsdetail);
+        //this.pagedetail.replace('src="/uploads/', 'src="http://rtarf2023.rtarf.mi.th:1337/uploads/'); 
       }else{
         this.pageshow='ไม่พบข้อมูล'
         this.pagedetail='ไม่พบข้อมูล'
